617f1597c5da3fcff602c135976335a7ed37a437
noritaka-hoshi4201 <noritaka-hoshi4201@genky.co.jp>
2024-12-10 15:08:28 +0900

    [update] setup

--- セットアップ1.bat
+++ セットアップ1.bat
@@ -5,5 +5,3 @@ cd /d %~dp0
 "%~dp0module\install_add_path.jpg"
 
 "%~dp0module\python-3.12.8-amd64.exe"
-
-cmd /k
--- セットアップ2.bat
+++ セットアップ2.bat
@@ -2,5 +2,5 @@ cd /d %~dp0
 
 start pip install scoop -t ./lib
 start pip install deap -t ./lib
-
-cmd /k
+start pip install pandas -t ./lib
+start pip install openpyxl -t ./lib
